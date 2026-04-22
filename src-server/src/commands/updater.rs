392bb8898c8597099d9d8411e8a7c1effeb31da6
霧雨バニラ <118162831+kirisamevanilla@users.noreply.github.com>
2026-04-22 13:06:11 +0800

    feat: auto update&rebind new port

--- src-server/src/commands/updater.rs
+++ src-server/src/commands/updater.rs
@@ -231,16 +231,12 @@ pub fn apply_update() -> Result<(), String> {
     let script_content = format!(
         r#"@echo off
 cd /d "%~dp0"
-timeout /t 2
 taskkill /f /im majdata-hub.exe
-timeout /t 2
+timeout /t 1
 copy /Y "%~dp0majdata-hub.new.exe" "%~dp0majdata-hub.exe"
-timeout /t 2
+timeout /t 1
 del "%~dp0majdata-hub.new.exe"
-timeout /t 2
 start majdata-hub.exe
-echo DONE
-pause
 "#,
     );
 
